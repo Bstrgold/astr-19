6175c08ae21a77bc19d6ed686e59bc75e02aeb42
Bstrgold <carelpunu@gmail.com>
2025-04-19 15:38:16 -0700

    Add files via upload

---
+++ s2prompt.py
@@ -0,0 +1,20 @@
+x = 5.7
+y = 3.2
+a = 10
+b = 4
+z = 2.5
+c = 6
+
+sum_result = x + y
+print("Sum of floats:", sum_result)
+print("Type:", type(sum_result))
+
+
+diff_result = a - b
+print("Difference of integers:", diff_result)
+print("Type:", type(diff_result))
+
+
+product_result = z * c
+print("Product of float and int:", product_result)
+print("Type:", type(product_result))
